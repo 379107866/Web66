81c322ff18ab7ed5e285b5b272f54d0825ad4f90
379107866 <379107866@qq.com>
2020-12-15 11:38:12 +0800

    it

---
+++ 第十五次课/apple.js
@@ -0,0 +1,10 @@
+function apple(){
+    var array=[12,67,4,43,3423,3]
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        console.log(element);
+    
+}
+}
+
+apple();
